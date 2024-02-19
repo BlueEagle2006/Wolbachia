4355b9f3e09ee3cc9f5aff2f7c0eee84bd1ffd0e
Can ER <can222er@gmail.com>
2024-02-19 22:04:03 +0300

    Code Rewriten

--- Function2.py
+++
@@ -1,9 +0,0 @@
-import math
-def function2(x):
-    return "%.58f" %(float(p*(1-(p**2)*(x**2)))/float((1-(p*x)+(p**2)*(x**2))**2));
-pler=[.8,.95,.75]
-for p in pler:
-    print(50*"="+" p="+str(float(p))+" "+50*"=")
-    xler=[0,float(1+math.sqrt((4*p)-3))/float(2*p),float(1-math.sqrt((4*p)-3))/float(2*p)]
-    for i in range(len(xler)):
-        print("x"+str(i+1)+": "+str(xler[i])+" ,\t\tfunction result:"+str(function2(xler[i])))
--- Wolbachia.py
+++ Wolbachia.py
@@ -1,20 +1,8 @@
-# -*- coding: utf-8 -*-
 import xlsxwriter,math
-xlsnum=0
-workbook = xlsxwriter.Workbook('Wolbachia.xlsx')
-worksheet = workbook.add_worksheet()
-alar=[.2,.5,.8,1,0,1,0]
-'''i=0
-while i<=1:
-    alar.append(float(i))
-    print(i)
-    i=float(i)+float(10**-2)
-'''#pler=[]
-bler=[.8,1,0,.2,.5,0,0.1]
-element=0
-genNum=input("Nesil Sayisi:")
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-def xlscol(num):
+import plotly.express as px
+import pandas as pd
+def xlscol(num): # The function that turns column numbers into xlsx understandable format [for example 0 -> A and 55 -> BD]
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     if num<len(alphabet):
         return alphabet[num]
     else:
@@ -22,70 +10,63 @@ def xlscol(num):
             for i in range(len(alphabet)):
                 if num<len(alphabet)*(i+2):
                     return str(alphabet[i])+str(alphabet[num-((i+1)*len(alphabet))])
-                '''elif num<len(alphabet)*(i+2)+(k+2)*len(alphabet):
-                    return str(alphabet[k])+str(alphabet[i])+str(alphabet[num-((i+1)*len(alphabet))])'''
-while True:
-    if element==len(alar):
-        break;
-    data = []
-    data2 = []
-    a=alar[element]
-    p=.90
-    #b=bler[element]
-    b=1-a
-    def f(gen):
-        if gen==0:
-            return float(a)
-        else:
-            return "%.53f" %((float(p)*n1)/(1-(p*n1)+((p**2)*(n1)**2)))
-    def f2(gen):
-        if gen==0:
-            return float(a)
-        elif gen==1:
-            return (float(a)*float(p))/(1-(float(p)*float(b))+((float(p)**2)*float(a)*float(b)))
-        else:
-            return "%.53f" %((float(p)*n1)/(1-(p*n1)+((p**2)*(n1)**2)))
-    for i in range(genNum+1):
-        print(str(i)+". Nesil :"+str(f(i)))
-        data.append([str(i),float(f(i))])
-        n1=float(f(i))
-    for i in range(genNum+1):
-        print(str(i)+". Nesil (f2):"+str(f2(i)))
-        data2.append([str(i),float(f2(i))])
-        n1=float(f2(i))
-    row = 1
-    col = (xlsnum*4)+1
-    worksheet.write(row, col, 'a:'+str(a))
-    worksheet.write(row, col+1, 'p:'+str(p))
-    worksheet.write(row, col+2, 'b=a')
-    row+=1
-    worksheet.write(row, col, 'Nesil')
-    worksheet.write(row, col+1, 'f(WolbachiaFemale)')
-    row+=1
-    for gen, value in (data):
-        worksheet.write(row, col,gen)
-        worksheet.write(row, col + 1, value)
-        row += 1
-    chart = workbook.add_chart({'type': 'line'})
-    chart.add_series({'values': '=Sheet1!$'+xlscol(col+1)+'$4:$'+xlscol(col+1)+'$'+str(4+genNum)})
-    chart.set_title({'name': 'a:'+str(a)+' p:'+str(p)+' b=a'})
-    worksheet.insert_chart(str(xlscol((len(alar)*4)+1))+str((element*15)+1), chart)
-    row+=2
-    worksheet.write(row, col, 'a:'+str(a))
-    worksheet.write(row, col+1, 'p:'+str(p))
-    worksheet.write(row, col+2, 'b:'+str(b))
-    row+=1
-    worksheet.write(row, col, 'Nesil')
-    worksheet.write(row, col+1, 'f(WolbachiaFemale)')
-    row+=1
-    for gen, value in (data2):
-        worksheet.write(row, col,gen)
-        worksheet.write(row, col + 1, value)
-        row += 1
-    chart = workbook.add_chart({'type': 'line'})
-    chart.add_series({'values': '=Sheet1!$'+xlscol(col+1)+'$'+str(4+genNum+5)+':$'+xlscol(col+1)+'$'+str(4+(genNum*2)+5)})
-    chart.set_title({'name': 'a:'+str(a)+' p:'+str(p)+' b:'+str(b)})
-    worksheet.insert_chart(str(xlscol((len(alar)*4)+10))+str((element*15)+1), chart)
-    xlsnum+=1
-    element+=1
-workbook.close()
+def f(x,a,d,w,b,fn): # The Wolbachia Percentage Calculator Function
+    x  = float(x)
+    a  = float(a)
+    d  = float(d)
+    w  = float(w)
+    b  = float(b)
+    fn = float(fn)
+    if x==0:
+        return a
+    elif x==1:
+        return (d*a) / (1-w*b+d*w*a*b)
+    else:
+        return (d*fn) / (1-w*fn+d*w*(fn**2))
+
+#=====The Values Are Here======
+list_a = [0.45,0.45]
+list_b = [0.07,0.1]
+list_d = [0.74]
+list_w = [1]
+max_x  = 30
+#==============================
+
+for i in range(len(list_a)):
+    a  = list_a[i]
+    b  = list_b[i]
+    for j in range(len(list_d)):
+        d  = list_d[j]
+        w  = list_w[j]
+        fn = 0
+        xlsx_data = []
+        xlsx_data_for_graph = []
+        id_data   = []
+        # Calculation of the "Wolbachia Percentage" until given generation value
+        for x in range(max_x):
+            #print(str(x)+". Generation : "+str(f(x,a,d,w,b,fn)))
+            xlsx_data.append([x,f(x,a,d,w,b,fn)])
+            xlsx_data_for_graph.append(f(x,a,d,w,b,fn))
+            id_data.append(x)
+            fn = f(x,a,d,w,b,fn)
+        # Creation of excel file
+        workbook = xlsxwriter.Workbook("a:"+str(a)+", d:"+str(d)+", w:"+str(w)+", b:"+str(b)+".xlsx")
+        worksheet = workbook.add_worksheet()
+        row = 1
+        col = 1
+        worksheet.write(row, col, 'Generation')
+        worksheet.write(row, col+1, 'Wolbachia')
+        row+=1
+        for x, fx in (xlsx_data):
+            worksheet.write(row, col,x)
+            worksheet.write(row, col + 1, fx)
+            row += 1
+        workbook.close()
+        # Creation of the graph
+        #DO NOT DELETE!!!
+        df = pd.DataFrame(dict(
+            Generation = id_data,
+            Wolbachia  = xlsx_data_for_graph
+        ))
+        fig = px.line(df, x="Generation", y="Wolbachia", title="a:"+str(a)+", d:"+str(d)+", w:"+str(w)+", b:"+str(b))
+        fig.write_image("a:"+str(a)+", d:"+str(d)+", w:"+str(w)+", b:"+str(b)+".png")
